23d93a3f94609fa58cb581ef8a72eb9b7d743b08
harlannorth <harlannorth@gmail.com>
2014-08-11 10:13:01 -0700

    Initial Code
    Initial code and unit tests

---
+++ OrderingWill/OrderingWill/OrderingWill.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace northboundsnails.tools
+{
+	//public interface ITree<T>
+	//{
+	//	IEnumerable<T> GetItems();
+
+	//	T GetItem();
+
+	//	void AddItem(T item);
+
+	//	void RemoveItem(T item);
+
+	//}
+
+}
+
+namespace OrderingWill
+{
+	public class Book
+	{
+		private IndexIterator _iterator;
+
+		private List<Tuple<int,int,IndexEntry>> unOrderedBook = new List<Tuple<int,int,IndexEntry>>();
+
+        public Book(IndexIterator iterator)
+		{
+
+			IndexEntry entry;
+			while((entry = iterator.GetNext()) != null)
+
+			unOrderedBook.Add(Tuple.Create<int,int,IndexEntry>(entry.Page,entry.Position, entry));
+		}
+
+		/// <summary>
+		/// returns a page in a book
+		/// </summary>
+		/// <param name="page">w</param>
+		/// <returns></returns>
+        public string GetPage(int page)
+		{
+			return string.Concat( unOrderedBook.Where(t => t.Item1 == page).OrderBy(t => t.Item2).Select(t => t.Item3.Word))
+		}
+
+	}
+
+	public class IndexEntry
+	{
+		public IndexEntry(int page, int position, string word)
+		{
+			Page = page;
+			Position = position;
+			Word = word;
+		}
+
+		public string Word;
+		public int Page;
+		public int Position;
+	}
+
+	public class IndexIterator
+	{
+
+		private string _uri;
+		private int position;
+
+		private string[] _fileLines;
+		private Func<string, IndexEntry> _entryParser;
+
+		public IndexIterator(string uri, Func<string,IndexEntry> createEntryParser):this(uri)
+		{
+			_entryParser = createEntryParser;
+		}
+
+		public IndexIterator(string uri)
+		{
+			position = 0;
+			_fileLines = File.ReadAllLines(uri);
+		}
+
+		private List<IndexEntry> Entries;
+		public IndexEntry GetNext()
+		{
+			IndexEntry entry = null;
+
+			//make sure there are still lines to read
+			if (position <= _fileLines.Count() - 1)
+			{
+					
+			}
+
+			return entry;
+
+			
+		}
+	}
+}
---
+++ OrderingWill/TestingWill/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace TestingWill
+{
+	[TestClass]
+	public class TestingWill
+	{
+
+
+
+		//sample from http://datahub.io/dataset/william-shakespeare-plays/resource/514d3c17-8469-4ae8-b83f-57678af50735
+		[TestMethod]
+		public void CanLoadAndIterate()
+		{
+
+			bool matches = true;
+
+			OrderingWill.IndexIterator iterator = new OrderingWill.IndexIterator(@"C:\Users\hnorth\Documents\GitHub\OrderingWill\OrderingWill\TestingWill\Data\HenryIV.csv");
+
+			//assert that each GetLine in iterator is the same as the one I do
+
+			string[] allLines = System.IO.File.ReadAllLines(@"C:\Users\hnorth\Documents\GitHub\OrderingWill\OrderingWill\TestingWill\Data\HenryIV.csv");
+
+			int i = 0;
+			do
+			{
+				
+				matches = iterator.GetNext().Word.Equals(allLines[i]);
+				i++;
+
+			} while (matches && i < allLines.Count());
+
+			Assert.IsTrue(matches);
+
+		}
+	}
+}
